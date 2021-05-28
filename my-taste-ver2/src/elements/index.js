974f03639eea49cf14559513d240fb15d7961599
ouo_yoonk <joyk9251@gmail.com>
2021-05-28 17:56:44 +0900

    signup page 수정

--- my-taste-ver2/src/elements/index.js
+++ my-taste-ver2/src/elements/index.js
@@ -5,5 +5,6 @@ import Button from './Button';
 import Input from './Input';
 import I from './I';
 import Wrapper from './Wrapper';
+import InputValid from './InputValid';
 
-export { Grid, Image, Text, Button, Input, I, Wrapper };
+export { Grid, Image, Text, Button, Input, I, Wrapper, InputValid };
--- my-taste-ver2/src/shared/common.js
+++ my-taste-ver2/src/shared/common.js
@@ -1,6 +1,24 @@
-const emailCheck = (email) => {
-  let _reg = /^[0-9a-zA-Z]([-_.0-9a-zA-Z])*@[0-9a-zA-Z]([-_.0-9a-z])*.([a-zA-Z])/;
-  return _reg.test(email);
+//패스워드 형식 체크 정규식
+export const testPwValid = (pw) => {
+  const _reg = /^(?=.*[a-zA-Z])((?=.*\d)|(?=.*\W)).{10,}$/;
+  return _reg.test(pw) && pw.search(/\s/) == -1 ? true : false;
 };
 
-export { emailCheck };
+// 패스워드 동일 숫자 반복 정규식
+export const testRepatNumber = (pw) => {
+  const _reg = /(\d)\1\1/;
+  return !_reg.test(pw);
+};
+
+// 이름 형식 체크 정규식
+export const testUsernameValid = (username) => {
+  let _reg = /^[a-zA-Z0-9ㄱ-ㅎ가-힣-_]{3,20}$/i;
+  return _reg.test(username);
+};
+
+//이메일 체크 정규식
+export const testEmailValid = (email) => {
+  let _reg =
+    /^[0-9a-zA-Z]([-_\.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-_\.]?[0-9a-zA-Z])*\.[a-zA-Z]{2,3}$/i;
+  return _reg.test(email);
+};
